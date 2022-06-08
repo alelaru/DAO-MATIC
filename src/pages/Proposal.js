caf3913dc5855731b18844aa63c2e5f410b9c1e4
Alelaru <alelaru@gmail.com>
2022-06-08 17:40:46 +0200

    UI done prepared to use Moralis and the Smart Contract to make it work

--- src/pages/Proposal.js
+++ src/pages/Proposal.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Blockie, Icon, Tag, Tooltip, Widget } from "web3uikit";
+import { Blockie, Form, Icon, Table, Tag, Tooltip, Widget } from "web3uikit";
 import "./pages.css";
 
 const Proposal = () => {
+
+  const [votes, setvotes] = useState([
+    [
+      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+      <Icon fill="#268c41" size={24} svg="checkmark" />,
+    ],
+    [
+      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+      <Icon fill="#268c41" size={24} svg="checkmark" />,
+    ],
+    [
+      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+      <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
+    ],
+    [
+      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+      <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
+    ],
+    [
+      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+      <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
+    ],
+  ]);
 
   return (
     <>
@@ -27,24 +50,59 @@ const Proposal = () => {
             </div>
           </div>
           <div className="widgets">
-            <Widget info={30} title="Votes for">
+            <Widget info={10} title="Votes for">
               <div className="extraWidgetInfo">
                 <div className="extraTitle">{75}%</div>
-                <div className="progress">
-                  <div className="progressPercentage" style={{ width:`${75}%`}}></div>
-                </div>
-              </div>
-            </Widget>
-            <Widget info={10} title="Votes against">
-              <div className="extraWidgetInfo">
-                <div className="extraTitle">{25}%</div>
                 <div className="progress">
                   <div className="progressPercentage" style={{ width:`${25}%`}}></div>
                 </div>
               </div>
             </Widget>
+            <Widget info={30} title="Votes against">
+              <div className="extraWidgetInfo">
+                <div className="extraTitle">{25}%</div>
+                <div className="progress">
+                  <div className="progressPercentage" style={{ width:`${75}%`}}></div>
+                </div>
+              </div>
+            </Widget>
+          </div>
+          <div className="votesDiv">
+            <Table
+              style={{ width: "60%"}}
+              columnsConfig="90% 10%"
+              data={votes}
+              header={[<span>Address</span>, <span>Vote</span>]}
+              pageSize={5}
+            ></Table>
+            <Form
+              style={{
+                width:"35%",
+                height:"250px",
+                border:"1px solid rgba(6,158,252,0.2)",
+              }}
+              buttonConfig={{
+                isLoading:false,
+                loadingText: "Casting Vote",
+                text:"vote",
+                theme:"secondary",
+              }}
+              data={[
+                {
+                  inputWidth:"100%",
+                  name:"Cast Vote",
+                  options:["For", "Against"],
+                  type: "radios",
+                  validation: {
+                    required: true
+                  },
+                }
+              ]}
+              onSubmit={e => alert('Vote cast')}
+            ></Form>
           </div>
      </div>
+     <div className="voting"></div>
     </>
   );
 };
