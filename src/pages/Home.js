632da116ce4898f5a5ce2b7b30e27f22a61f28e9
Alelaru <alelaru@gmail.com>
2022-06-08 16:58:45 +0200

    Recent Proposals ui created in the Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { Form, Tab, Table, TabList, Widget } from "web3uikit";
+import { Form, Tab, Table, TabList, Tag, Widget } from "web3uikit";
+import { Link } from "react-router-dom";
 import "./pages.css";
 
 
 const Home = () => {
+
+  const [proposals, setProposals] = useState(
+
+    [
+      [
+        1,
+        <div>Should we start a Moralis hamburger chain?</div>,
+        <Tag color="green" text="Passed" />,
+      ],
+      [
+        2,
+        "Should we accept Elon Musks $44billion offer for our DAO?",
+        <Link to="/proposal" state={"hello"}>
+          <Tag color="red" text="Rejected" />
+        </Link>,
+      ],
+      [
+        3,
+        "Do you want a Web3 Slack tutorial?",
+        <Tag color="blue" text="Ongoing" />,
+      ],
+      [
+        4,
+        "Are you interested in Xbox/Console web3 tutorials?",
+        <Tag color="blue" text="Ongoing" />,
+      ],
+      [
+        5,
+        "Would you attend a Moralis Builder get together in Miami?",
+        <Tag color="blue" text="Ongoing" />,
+      ],
+  ]
+
+  );
  
   return (
     <>
@@ -17,10 +52,33 @@ const Home = () => {
                   info={52}
                   title="Proposal created"
                   style={{ width: "200%"}}
-                ></Widget>
+                >
+                  <div className="extraWidgetInfo">
+                    <div className="extraTitle">Pass Rate</div>
+                    <div className="progress">
+                      <div className="progressPercentage" style={{ width: `${60}%`}}></div>
+                    </div>
+                  </div>
+
+                </Widget>
                 <Widget info={243} title="Elegible Voters"></Widget>
                 <Widget info={5} title="Ongoing Proposals"></Widget>
               </div>
+              Recent Proposals
+                <div style={{marginTop: "30px"}}>
+                  <Table
+                    columnsConfig="10% 70% 20%"
+                    data={proposals}
+                    header={[
+                      <span>ID</span>,
+                      <span>Description</span>,
+                      <span>Status</span>
+
+                    ]}
+                    pageSize={5}
+                  ></Table>
+                </div>
+
             </div>
           </Tab>
           <Tab tabKey={2} tabName="Forum"></Tab>
